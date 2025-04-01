08164071e4b45949c92ef24788046d35e6b729ce
MarcoSchenker <mschenker@mail.austral.edu.ar>
2025-04-01 12:18:19 -0300

    Changes in pages

---
+++ frontend/src/components/HeaderDashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <div className="header-component">
+      <Link to="/dashboard" className="header-link">
+        <img src="/CardLogo.png" alt="Logo" className="logo" />
+        <h1 className="title">Trucho</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Header;
