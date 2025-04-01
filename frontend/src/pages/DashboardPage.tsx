f2296c4a6015ba998b52cbf4380b61d437532b28
ignaciogaspar <igaspar@mail.austral.edu.ar>
2025-04-01 12:28:09 -0300

    Co-authored-by: Marco <MarcoSchenker@users.noreply.github.com>

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaCoins, FaSignOutAlt, FaMedal, FaQuestionCircle } from 'react-icons/fa';
 import Header from '../components/Header';
 import './DashboardPage.css';
 
 const DashboardPage: React.FC = () => {
+  const userImage = 'frontend/public/foto_anonima.jpg'; // Ruta de imagen anónima
+  const userElo = 1200;
+  const userCoins = 500;
+
   return (
     <div className="dashboard-container">
       <Header />
-      <hr className="line-white" />
-      <div className="dashboard-content">
-        <h2>Bienvenido a tu Dashboard</h2>
-        <div className="button-container">
-          <Link to="/jugar">
-            <button className="dashboard-button">Jugar</button>
-          </Link>
-          <Link to="/amigos">
-            <button className="dashboard-button">Amigos</button>
-          </Link>
-          <Link to="/perfil">
-            <button className="dashboard-button">Perfil</button>
-          </Link>
+      <div className="top-left-help">
+        <FaQuestionCircle className="icon" title="Ayuda" />
+      </div>
+      <div className="top-right-icons">
+        <div className="profile-info">
+          <div className="profile-icon" style={{ backgroundImage: `url(${userImage})` }}></div>
+          <FaSignOutAlt className="icon" title="Cerrar sesión" />
+          <div className="icon-text"><FaCoins /> {userCoins}</div>
+          <div className="icon-text"><FaMedal /> {userElo}</div>
         </div>
+      </div>
+      <hr className="line-white" />
+      <div className="game-modes">
+        <Link to="/jugar-offline" className="game-mode">
+          <div>Jugar Offline</div>
+        </Link>
+        <Link to="/torneo" className="game-mode">
+          <div>Torneo</div>
+        </Link>
+        <Link to="/salas" className="game-mode">
+          <div>Salas (2, 4, 6 Jugadores)</div>
+        </Link>
       </div>
     </div>
   );
