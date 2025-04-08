1090536bb8fadab389d9bc59df43635d572c23ce
ignaciogaspar <igaspar@mail.austral.edu.ar>
2025-04-08 09:37:11 -0300

    Changes in front: home, login and register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import StatsPage from './pages/StatsPage';
 import DashboardPage from './pages/DashboardPage';
@@ -15,7 +14,6 @@ function App() {
       <Routes>
         {/* Rutas públicas */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/jugar-offline" element={<JugarOffline />} />
 
