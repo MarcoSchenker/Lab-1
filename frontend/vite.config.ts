8b0d30c794aa5ba9dcb43440ba6f10bf9df92f65
MarcoSchenker <mschenker@mail.austral.edu.ar>
2025-10-28 01:14:56 -0300

    activating vite for deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ const backendUrl = process.env.VITE_API_URL || 'http://localhost:3001';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // Escuchar en todas las interfaces de red
+    port: 5173,
+    strictPort: true,
     proxy: {
       '/api': {
         target: backendUrl,
